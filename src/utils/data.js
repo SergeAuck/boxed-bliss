36983bd26c2620991926280f468fada743cc6a37
SergeAuck <sergey.auckland@gmail.com>
2023-06-26 20:42:40 +1200

    styles + data

---
+++ src/utils/data.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const navLinks = [
+  {
+    id: 1,
+    text: "home",
+    url: "/",
+  },
+  {
+    id: 2,
+    text: "about",
+    url: "/about",
+  },
+  {
+    id: 3,
+    text: "products",
+    url: "/products",
+  },
+];
