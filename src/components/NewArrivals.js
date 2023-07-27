461d4861adc9438ead28c2b3583c37f72e3cf4f3
SergeAuck <sergey.auckland@gmail.com>
2023-07-27 20:52:33 +1200

    New arrivals update

--- src/components/NewArrivals.js
+++ src/components/NewArrivals.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useProductsContext } from "../context/products_context";
+import { Link } from "react-router-dom";
 import Error from "./Error";
 import Loading from "./Loading";
 import styled from "styled-components";
@@ -26,6 +27,9 @@ const NewArrivals = () => {
           return <Product key={product.id} {...product} />;
         })}
       </div>
+      <Link to="/products" className="btn">
+        All Games
+      </Link>
     </Wrapper>
   );
 };
